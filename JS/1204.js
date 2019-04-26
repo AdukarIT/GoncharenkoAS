f3512eb5716590ae9610be3819876ad7f74c31d9
Andrei <co4hbly@yandex.by>
2019-04-26 12:52:29 +0300

    js

---
+++ JS/1204.js
@@ -0,0 +1,11 @@
+let table = document.getElementsByTagName('table');
+console.log(table);
+
+console.log(table[0].getElementsByTagName('label'));
+
+console.log(table[0].getElementsByTagName('td')[0]);
+
+console.log(document.getElementsByName('search-person')[0]);
+
+let form = document.querySelector('form[name="search"]');
+console.log(form);
